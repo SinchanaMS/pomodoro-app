85cce789384e443ffd026c1ef82dab57ca3dc775
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-05-04 23:25:44 +0530

    fix: added default exports

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,4 +1,4 @@
-export { HomePage } from "pages/HomePage";
-export { PageNotFound } from "pages/PageNotFound";
-export { SingleTask } from "pages/SingleTask";
-export { TasksList } from "pages/TasksList";
+export { default as HomePage } from "./HomePage";
+export { default as PageNotFound } from "./PageNotFound";
+export { default as SingleTask } from "./SingleTask";
+export { default as TasksList } from "./TasksList";
